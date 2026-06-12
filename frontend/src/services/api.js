8076d6a5d605c428fc18793e8bfe336e9dcf8e2e
satyajit <satyajitpraharaj51@gmail.com>
2026-06-12 18:11:00 +0530

    Fix API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://library-management-system-sche.onrender.com"
+  baseURL: "https://library-management-system-sche.onrender.com/api"
 });
 
 export default API;
